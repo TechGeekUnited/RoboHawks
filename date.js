45b29ccdda66dbb69bce20022361ff6b9c0e9217
RoboHawks <RoboHawks.Dev@gmail.com>
2022-08-28 15:36:30 -0700

    Create date.js

---
+++ date.js
@@ -0,0 +1,7 @@
+let currentDate = new Date();
+let cDay = currentDate.getDate()
+let cMonth = currentDate.getMonth() + 1
+let cYear = currentDate.getFullYear()
+console.log(cDay);
+console.log(cMonth);
+console.log(cYear);
